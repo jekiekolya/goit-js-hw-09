aa3e81a6220c5bc84d17f7d1dfc082e8b180867e
jekiekolya <108493349+jekiekolya@users.noreply.github.com>
2022-09-23 15:25:30 +0000

    Deploying to gh-pages from @ jekiekolya/goit-js-hw-09@f8e9fd72e4061a198c60fc6a276b60884ce12cae 🚀

---
+++ 03-promises.526092c8.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".form")}).form.addEventListener("submit");
+//# sourceMappingURL=03-promises.526092c8.js.map
---
+++ 03-promises.a47d1467.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit");
+//# sourceMappingURL=03-promises.a47d1467.js.map
